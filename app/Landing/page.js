b0e8a96ec44682d9ec617e7d6bceaea64f60f669
iamkorir3 <korir5956@gmail.com>
2025-12-01 23:52:49 +0300

    initialiasing the landing page

---
+++ app/Landing/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Landing() {
+  return (
+    <div>
+      <div>
+        <h1>Job Solutions</h1>
+        <p>Connect to a job easily</p>
+      </div>
+      <div>
+        <Image src="" alt="prof pic" />
+      </div>
+    </div>
+  );
+}
