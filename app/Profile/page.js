a2b4496bc57f74c3a68fe5eb256198abdab784d6
iamkorir3 <korir5956@gmail.com>
2025-12-08 23:57:17 +0300

    user progile

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -1,3 +1,17 @@
 export default function Profile() {
-  return <div>user Profile Page</div>;
+  const user = {
+    nme: "korir",
+    age: 24,
+    img: "korir.jpg",
+    email: "korir@gmail.com",
+  };
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <h2>Name: {user.nme}</h2>
+      <h3>Age: {user.age}</h3>
+      <h3>Email: {user.email}</h3>
+      <img src={user.img} alt="Profile Picture" />
+    </div>
+  );
 }
