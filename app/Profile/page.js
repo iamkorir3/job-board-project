b39c3c3db78e02c891a04858092dcb8d425863f3
iamkorir3 <korir5956@gmail.com>
2025-12-12 23:59:27 +0300

    profile

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -2,16 +2,15 @@ export default function Profile() {
   const user = {
     nme: "korir",
     age: 24,
-    img: "korir.jpg",
+    img: "ui.jpg",
     email: "korir@gmail.com",
   };
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className={profileContainer}>
+      <img src={user.img} alt="Profile Picture" />
       <h2>Name: {user.nme}</h2>
       <h3>Age: {user.age}</h3>
       <h3>Email: {user.email}</h3>
-      {/* <img src={user.img} alt="Profile Picture" /> */}
     </div>
   );
 }
