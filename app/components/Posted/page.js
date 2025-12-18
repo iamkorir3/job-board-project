22cb06d05c7a2b5d8b7c671647aeba58d617a1b6
iamkorir3 <korir5956@gmail.com>
2025-12-18 21:10:55 +0300

    posted jobs

---
+++ app/components/Posted/page.js
@@ -0,0 +1,19 @@
+function Posted() {
+  return (
+    <div>
+      <JobList />
+    </div>
+  );
+}
+function JobList() {
+  return (
+    <div>
+      <ul>
+        <li>Job one</li>
+        <li>Job two</li>
+        <li>Job three</li>
+      </ul>
+    </div>
+  );
+}
+export default Posted;
