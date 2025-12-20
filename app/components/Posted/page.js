0c9a7d22e948ccb73b0c9a5cf81eb4c0e863fb2f
iamkorir3 <korir5956@gmail.com>
2025-12-20 19:53:37 +0300

    posted jobs options

--- app/components/Posted/page.js
+++ app/components/Posted/page.js
@@ -24,10 +24,7 @@ const jobs = [
     title: "job one",
     type: "full time",
   },
-  {
-    title: "job one",
-    type: "full time",
-  },
+  { title: "job one", type: "full time" },
 ];
 
 function Posted() {
@@ -39,17 +36,26 @@ function Posted() {
     </div>
   );
 }
+
 function JobList({ job }) {
   const { type, title } = job;
+
+  function showOptions() {}
+
   return (
     <div className={styles.job}>
       <ul>
         <li>
           <div>
             <h3>{title}</h3>
-            <button>...</button>
+            <button onClick={showOptions}>...</button>
           </div>
           <p>{type}</p>
+          <div className={styles.options}>
+            <button></button>
+            <button>Visit</button>
+            <button>Do not show</button>
+          </div>
         </li>
       </ul>
     </div>
