a39070d0eb912c77e90ee86edb8174b819ac2ccb
iamkorir3 <korir5956@gmail.com>
2025-12-25 23:27:18 +0300

    job

--- app/components/Jobs/page.js
+++ app/components/Jobs/page.js
@@ -1,3 +1,13 @@
 export default function Jobs() {
   return <div>jobs</div>;
 }
+
+function Job() {
+  return (
+    <div>
+      <h1>title</h1>
+      <p>description</p>
+      <button>View</button>
+    </div>
+  );
+}
