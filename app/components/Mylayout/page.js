f0916d10120962ca9fa5f598a403b50945ac93bf
thordoer <mulaivan744@gmail.com>
2025-12-30 21:03:55 +0300

    jobs

--- app/components/Mylayout/page.js
+++ app/components/Mylayout/page.js
@@ -1,9 +1,5 @@
 import Dashboard from "../../DashBoard/page";
-import styles from "./Dashboard.module.css";
-import Nav from "../components/TestNav/page.js";
-import Profile from "../Profile/page.js";
-import Stats from "../components/Stats.js";
-import Posted from "../components/Posted/page.js";
+
 function Mylayout() {
   return (
     <div>
