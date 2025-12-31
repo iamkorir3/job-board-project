a75cbdf988c81eeaddcb57434dec8af7778a4225
iamkorir3 <korir5956@gmail.com>
2025-12-31 23:22:25 +0300

    helpers

---
+++ app/Helpers/PostJob.js
@@ -0,0 +1,10 @@
+const jobs = [
+  { titlte: "korir", time: "22", description: "lorem ipsum" },
+  { titlte: "korir", time: "22", description: "lorem ipsum" },
+  { titlte: "korir", time: "22", description: "lorem ipsum" },
+  { titlte: "korir", time: "22", description: "lorem ipsum" },
+];
+
+const PostJob = (job) => {
+  [job, ...jobs];
+};
