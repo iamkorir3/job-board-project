878622f8d89f96558d7597e7f07d94ed98dc6c0d
Korir emmanuel <korir5956@gmail.com>
2026-01-26 22:47:33 +0300

    planning

--- app/Helpers/Logout.js
+++ app/Helpers/Logout.js
@@ -1,8 +1,10 @@
 import { useRouter } from "next/navigation";
+//i will have to get the user seesion
 export const useLogout = () => {
   const router = useRouter();
 
   const logout = () => {
+    //and clear user session
     localStorage.removeItem("token");
     router.push("/login");
   };
