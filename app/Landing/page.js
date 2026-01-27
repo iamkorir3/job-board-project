479d4703cc174ded2d9668732f5248601a00548a
Korir emmanuel <korir5956@gmail.com>
2026-01-27 21:48:54 +0300

    Fix typo in header from 'Job Solutions' to 'Jobs Solution'

--- app/Landing/page.js
+++ app/Landing/page.js
@@ -7,7 +7,7 @@ export default function Landing() {
   return (
     <div className={styles.container}>
       <div>
-        <h1>Job Solutions</h1>
+        <h1>Jobs Solution</h1>
         <p>Connect to a job easily</p>
       </div>
       <div>
