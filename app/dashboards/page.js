ae17a62e74812fc00c4f148fec24a246a75df3ca
Korir emmanuel <korir5956@gmail.com>
2026-02-03 23:05:43 +0300

    styles import and class name in DashBoards
    Fix import statement and update class name in DashBoards component.

--- app/dashboards/page.js
+++ app/dashboards/page.js
@@ -1,8 +1,10 @@
 import Stats from "../components/Stats";
+import styles fron "./DashBoards.module.css";
 
 export default function DashBoards() {
+  //dashboard funstions
   return (
-    <div>
+    <div className={styles.dasgContainer}>
       <Stats />
     </div>
   );
