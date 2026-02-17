cb8a68c96cdba87777b6637eb1cb15283f610ccc
Korir emmanuel <korir5956@gmail.com>
2026-02-17 14:50:33 -0500

    Implement  simple user session management in logout
    Added user session handling in the logout function.

--- app/Helpers/Logout.js
+++ app/Helpers/Logout.js
@@ -2,9 +2,12 @@ import { useRouter } from "next/navigation";
 //i will have to get the user seesion
 export const useLogout = () => {
   const router = useRouter();
+  //getting user session
+  const session = usersession;
 
   const logout = () => {
     //and clear user session
+    session = null;
     localStorage.removeItem("token");
     router.push("/login");
   };
