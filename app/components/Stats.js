4668163fcf020506ec12769f45fd277299f94f63
Korir emmanuel <korir5956@gmail.com>
2026-02-23 15:23:52 -0500

    Display the number of jobs dynamically in Stats

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -1,10 +1,11 @@
 import styles from "./Stats.module.css";
 function Stats() {
+  const numberOfJobs = jobs.length;
   return (
     <div className={styles.statsContainer}>
       <div className={styles.statBox}>
         <div>
-          <h2>12</h2>
+          <h2>{numberOfJobs}</h2>
           <p>jobs posted</p>
         </div>
         <div>
